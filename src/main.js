fcf97d16e5a5f66aed85ba26d75c6a0d1860cae5
kennethlindalen <kennethlindalen@gmail.com>
2021-01-05 06:08:32 +0100

    Complete re-write

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
+import VueX from 'vuex'
 import App from './App.vue'
+import store from './store/store.js'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(VueX);
 
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
+
+
